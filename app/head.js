b5d6199d28685ebb96483afc57e0250e0f6863aa
Saransh29 <saranshbibiyan234@gmail.com>
2023-03-12 04:42:15 +0530

    added og-image

--- app/head.js
+++ app/head.js
@@ -3,8 +3,15 @@ export default function Head() {
     <>
       <title>OpenJourney</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Generate imaginative images through Stable Diffusion AI - midjourney v4 Model" />
+      <meta
+        name="description"
+        content="Generate imaginative images through Stable Diffusion AI - midjourney v4 Model"
+      />
       <link rel="icon" href="/favicon.ico" />
+      <meta
+        property="og:image"
+        content="https://next-stable-diff.vercel.app/og.png"
+      />
     </>
-  )
+  );
 }
